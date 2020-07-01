a8531d0ca0ad2779c9160359e4453f2ae12f903d
gad <mohamed.gad@insidetrack.com>
2020-07-02 00:17:47 +0200

    xray reports edits

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   resources :vists
   resources :x_rays
+  get '/x_rays/:x_ray_id/reports/new', to: 'reports#new'
+  post '/x_rays/:x_ray_id/reports/', to: 'reports#create', as: :create_xray_report
   resources :reports
   resources :paitents
   devise_for :technicians
